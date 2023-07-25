6d8b5a6b2dcb874d258bebad3dd7ef856e49d2e9
AlexGoIT <alexandrpuryaev@gmail.com>
2023-07-25 22:00:40 +0300

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,24 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 
-// import LocalStorageAPI from 'services/localStorageAPI';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
 
-// const lsAPI = new LocalStorageAPI();
-const KEY = 'phonebook-contacts';
+import useLocalStorage from 'hooks/useLocalStorage';
 
 window.document.title = 'HW-4 Phonebook';
 
+const KEY = 'phonebook-contacts';
 const defaultContacts = [
   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
-
-const useLocalStorage = (key, initialValue) => {
-  const [state, setState] = useState(() => {
-    return JSON.parse(window.localStorage.getItem(key)) ?? initialValue;
-  });
-
-  useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(state));
-  }, [key, state]);
-
-  return [state, setState];
-};
 
 const App = () => {
   const [contacts, setContacts] = useLocalStorage(KEY, defaultContacts);
